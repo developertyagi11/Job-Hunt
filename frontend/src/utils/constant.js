bd299bac485371506e12c6d444a01c56855b0a05
Lokesh-techcrumb <lokesh@techcrumb.com>
2025-02-08 23:38:34 +0530

    Complete Project

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://job-junction-s6vn.onrender.com/user";
-export const JOB_API_END_POINT="https://job-junction-s6vn.onrender.com/job";
-export const APPLICATION_API_END_POINT="https://job-junction-s6vn.onrender.com/application";
-export const COMPANY_API_END_POINT="https://job-junction-s6vn.onrender.com/company";
+export const USER_API_END_POINT="https://job-hunt-1-8uic.onrender.com/user";
+export const JOB_API_END_POINT="https://job-hunt-1-8uic.onrender.com/job";
+export const APPLICATION_API_END_POINT="https://job-hunt-1-8uic.onrender.com/application";
+export const COMPANY_API_END_POINT="https://job-hunt-1-8uic.onrender.com/company";
